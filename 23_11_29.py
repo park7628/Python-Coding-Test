c41c32a2528344d7a39411d5411b237a89bcf071
park7628 <108246145+park7628@users.noreply.github.com>
2023-11-29 22:23:12 +0900

    Create 23_11_29.py

---
+++ 23_11_29.py
@@ -0,0 +1,32 @@
+# 35P
+# a = int(input())
+
+# numlist = list(input())
+
+# sum = 0
+
+# for i in numlist:
+#     sum += int(i)
+    
+
+# print(sum)
+
+####################
+# 38p
+# n = int(input())
+# score = list(map(int, input().split()))
+
+# # print (score)
+
+# maxScore = max(score)
+
+# for i in range(len(score)):
+#     score[i] = score[i] / maxScore * 100
+
+# # print(score)
+
+
+# sumScore = sum(score)
+# arr = sumScore / len(score)
+
+# print(arr)
